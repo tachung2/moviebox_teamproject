5fb9f00aa04d4d1800e24c1c01470b77d956d896
tachung2 <40621278+tachung2@users.noreply.github.com>
2021-09-26 15:25:59 +0900

    initial commit

--- src/index.js
+++ src/index.js
@@ -4,22 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const formatName = function(name) {
-    return name.firstName + " " + name.lastName;
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+
+ReactDOM.render(
+  element,
+  document.getElementById('root'));
 }
 
-const name = {
-    firstName: "jemin",
-    lastName: "Sung",
-};
-const element = <h1> Hello,{ formatName(name) }! </h1>
-
-ReactDOM.render( <React.StrictMode>
-    <App/>
-    </React.StrictMode>, document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+setInterval(tick,1000);
