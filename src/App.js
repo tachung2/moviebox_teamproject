462820bf7a23f596a21f14ee2c4b354dddfdf506
tachung2 <40621278+tachung2@users.noreply.github.com>
2021-09-26 17:03:53 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,25 +3,13 @@ import './App.css';
 import Commnet from './Comment'
 
 function App() {
+  let posts = '영화';
   return (
-    <div className="App" style={{ padding: 16, backgroundColor: '#282c34'}}>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div>
-      <Commnet />
+    <div className="App">
+      <div className="black-nav">
+        <div>Moviebox</div>
       </div>
+      
     </div>
   );
 }
