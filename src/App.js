1d4fbd9f19a36bd16d2e36ea9c3e41f09256bedf
tachung2 <40621278+tachung2@users.noreply.github.com>
2021-09-26 17:51:33 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+/*eslint-disable*/
+
+import React, { useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
-import Commnet from './Comment'
+import './App.css'
 
 function App() {
-  let posts = '영화';
+
+  let [영화, 리스트] = useState(['뭔가', '없다']);
+  let [a,b] = usestate(0);
+
   return (
     <div className="App">
       <div className="black-nav">
         <div>Moviebox</div>
       </div>
-      
+      <div className = "list">
+        <h3> { 영화[0] } <span onclick={()=>{ b(a+1) } }> 좋아요 </span> 0 </h3>
+        <hr/>
+      </div>
     </div>
   );
 }
