b9183ac002ebaa77ccefd3a550df6699da85efc9
tachung2 <40621278+tachung2@users.noreply.github.com>
2021-09-26 16:57:11 +0900

    initial commit

--- src/index.js
+++ src/index.js
@@ -4,20 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
-}
-
-function Wpp() {
-  return (
-    <div>
-      <Welcome name="tachung" />
-      <Welcome name="onder" />
-      <Welcome name="maki" />
-    </div>
-  );
-}
-
 ReactDOM.render(
-  <Wpp />,
-  document.getElementById('root'));
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
