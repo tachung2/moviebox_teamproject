551b50e9558d44939bb65efdccc0e3752c36fba9
tachung2 <40621278+tachung2@users.noreply.github.com>
2021-09-26 14:48:24 +0900

    initial commit

--- src/index.js
+++ src/index.js
@@ -4,11 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const formatName = function(name) {
+    return name.firstName + " " + name.lastName;
+}
+
+const name = {
+    firstName: "jemin",
+    lastName: "Sung",
+};
+const element = <h1> Hello,{ formatName(name) }! </h1>
+
+ReactDOM.render( <React.StrictMode>
+    <App/>
+    </React.StrictMode>, document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
